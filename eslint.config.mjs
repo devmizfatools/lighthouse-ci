28c39299d5b0214327bd7a2502965f6baa3a044c
devmizfa <devmizfatools@gmail.com>
2024-06-30 12:05:59 +0330

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,23 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module",
+      ecmaVersion: 2020, // تنظیم نسخه ECMAScript
+      globals: {
+        ...globals.browser,
+        ...globals.node, // اضافه کردن globals مربوط به Node.js
+        ...globals.jest, // اضافه کردن globals مربوط به Jest
+      },
+    },
+  },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      // قوانین مورد نظر خود را اینجا اضافه کنید
+    },
+  },
 ];
